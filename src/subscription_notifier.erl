73f88bfe2d0a294f63fa9b66175e7a9d364728ed
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-28 13:35:41 +0100

    Adding super trivial implementation of subscription_notifier.erl

--- src/subscription_notifier.erl
+++ src/subscription_notifier.erl
@@ -1,8 +1,23 @@
 -module(subscription_notifier).
 -export([process/1]).
 
+-define(BASEURL, "http://yourcallback.com").
+
 -include("utils.hrl").
 
-process(#subscription{appid=_AppId, userid=_UserId, regid=_RegId} = _Subscription) ->
+process(#subscription{appid=_AppId, userid=UserId, regid=_RegId} = _Subscription) ->
     error_logger:info_msg("Notify external application of subscription~n", []),
-    ok.
+    Json = jsx:encode(#{<<"userId">> => UserId}),
+    try httpc:request(post, {?BASEURL, [], "application/json", Json}, [], []) of
+        {ok, {{_, 200, _}, _Headers, _Body}} ->
+            error_logger:info_msg("Subscription successfully notified~n", []);
+        {ok, {{_, _, _}, _, _}} ->
+	    error_logger:error_msg("Error in request.~n", []);
+        {error, Reason} ->
+	    error_logger:error_msg("Error in request. Reason was: ~p~n", [Reason]);
+        OtherError ->
+	    error_logger:error_msg("Error in request. Reason was: ~p~n", [OtherError])
+    catch
+        Exception ->
+	    error_logger:error_msg("Error in request. Exception ~p~n", [Exception])
+    end.
