e9c99019600440a8e7ddaf1164a5434ad0af562b
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-29 11:46:42 +0100

    Adding retry after mechanism

--- src/subscription_notifier.erl
+++ src/subscription_notifier.erl
@@ -1,26 +1,40 @@
 -module(subscription_notifier).
--export([process/1]).
+-export([process/1, process/3]).
 
 -define(BASEURL, "http://yourcallback.com").
+-define(MAX_RETRY, 5).
+-define(RETRY_AFTER, 5000).
 
 -include("utils.hrl").
 
-process(#subscription{appid=_AppId, userid=UserId, regid=_RegId} = _Subscription) ->
+process(Subscription) ->
+    process(Subscription, ?RETRY_AFTER, 0).
+
+process(_, _, ?MAX_RETRY) ->
+    ok;
+
+process(Subscription, RetryAfter, RetryNumber) ->
     error_logger:info_msg("Notify external application of subscription~n", []),
+    #subscription{appid=_AppId, userid=UserId, regid=_RegId} = Subscription,
     Json = jsx:encode(#{<<"userId">> => UserId}),
     try httpc:request(post, {?BASEURL, [], "application/json", Json}, [], []) of
         {ok, {{_, 200, _}, _Headers, _Body}} ->
             error_logger:info_msg("Subscription successfully notified~n", []);
         {ok, {{_, _, _}, _, _}} ->
-            %%TODO: retry after a while for n-times
-	    error_logger:error_msg("Error in request.~n", []);
+	    error_logger:error_msg("Error in request.~n", []),
+            do_backoff(Subscription, RetryAfter, RetryNumber);
         {error, Reason} ->
-            %%TODO: retry after a while for n-times
-	    error_logger:error_msg("Error in request. Reason was: ~p~n", [Reason]);
+	    error_logger:error_msg("Error in request. Reason was: ~p~n", [Reason]),
+            do_backoff(Subscription, RetryAfter, RetryNumber);
         OtherError ->
-            %%TODO: retry after a while for n-times
-	    error_logger:error_msg("Error in request. Reason was: ~p~n", [OtherError])
+	    error_logger:error_msg("Error in request. Reason was: ~p~n", [OtherError]),
+            do_backoff(Subscription, RetryAfter, RetryNumber)
     catch
         Exception ->
-	    error_logger:error_msg("Error in request. Exception ~p~n", [Exception])
+	    error_logger:error_msg("Error in request. Exception ~p~n", [Exception]),
+            do_backoff(Subscription, RetryAfter, RetryNumber)
     end.
+
+do_backoff(Subscription, RetryAfter, RetryNumber) ->
+    timer:apply_after(RetryAfter * 2, ?MODULE, process, [Subscription, RetryAfter * 2, RetryNumber + 1]),
+    ok.
