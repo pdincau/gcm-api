81f7e51de9301ed1b215148dd51255bd8753f744
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-29 19:55:38 +0100

    wiring gcm-app. This will be reviewed when introducing multi-app support

--- src/notification_handler.erl
+++ src/notification_handler.erl
@@ -4,6 +4,8 @@
 -export([handle/2]).
 -export([terminate/3]).
 
+-include("utils.hrl").
+
 init(_Transport, Req, []) ->
     {ok, Req, undefined}.
 
@@ -15,9 +17,9 @@ handle(Req, State) ->
 
 handle_request(<<"POST">>, true, Req) ->
     {ok, [{Payload, true}], Req2} = cowboy_req:body_qs(Req),
-    error_logger:info_msg("Received notification: ~p~n", [Payload]),
-    process(jsx:decode(Payload)),
-    cowboy_req:reply(201, [], <<"">>, Req2);
+    {AppName, Req3} = cowboy_req:binding(app_name, Req2),
+    process(binary_to_existing_atom(AppName, utf8), jsx:decode(Payload)),
+    cowboy_req:reply(201, [], <<"">>, Req3);
 
 handle_request(<<"POST">>, false, Req) ->
     cowboy_req:reply(400, [], <<"{\"error\":\"missing body\"}">>, Req);
@@ -28,6 +30,15 @@ handle_request(_, _, Req) ->
 terminate(_Reason, _Req, _State) ->
     ok.
 
-process(Update) ->
-    Fun = fun([{UserId, [{Key, Value}]}]) -> io:format("~p ~p ~p ~n", [UserId, Key, Value]) end,
-    lists:map(Fun, Update).
+process(AppName, Update) ->
+    error_logger:info_msg("Processing notifications: ~p~n", [Update]),
+    lists:map(fun(Notification) -> send(AppName, Notification) end, Update).
+
+send(AppName, [{UserId, [{Key, Value}]}]) ->
+    case subscriptions:find(UserId) of
+        {error, _} ->
+            error_logger:error_msg("UserId: ~p not found.~n", [UserId]);
+        {ok, Subscription} ->
+            Message = [{<<"data">>, [{Key, Value}]}],
+            gcm:push(AppName, [Subscription#subscription.regid], Message)
+    end.
