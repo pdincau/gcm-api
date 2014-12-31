c13bd1050129688dff30efe18f9a89c9301bcf7b
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-31 14:19:16 +0100

    Passing appname as a variable

--- src/gcm_api_sup.erl
+++ src/gcm_api_sup.erl
@@ -10,12 +10,12 @@
 
 -define(SERVER, ?MODULE).
 
--define(CHILD(I, Type), {I, {I, start_link, [<<"appname">>]}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, AppName), {I, {I, start_link, [AppName]}, permanent, 5000, Type, [I]}).
 
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
-    Procs = [?CHILD(subscriptions, worker)],
+    Procs = [?CHILD(subscriptions, worker, <<"appname">>)],
     {ok, {{one_for_one, 0, 1}, Procs}}.
 
