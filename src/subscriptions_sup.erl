a3e73f7beeeb44ae1f9443f83f691b85027dc5ab
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-01-04 15:33:03 +0100

    Improving supervisor

--- src/subscriptions_sup.erl
+++ src/subscriptions_sup.erl
@@ -8,12 +8,10 @@
 %% Supervisor callbacks
 -export([init/1]).
 
--define(SERVER, ?MODULE).
-
 -define(CHILD(I, Type, AppName), {I, {I, start_link, [AppName]}, permanent, 5000, Type, [I]}).
 
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
     Procs = [?CHILD(subscriptions, worker, <<"appname">>)],
