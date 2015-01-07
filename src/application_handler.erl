b69e0c324b1f8f077609715294ffd44dba02e6a8
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-01-07 23:31:15 +0100

    Adding base application_handler.erl

---
+++ src/application_handler.erl
@@ -0,0 +1,35 @@
+-module(application_handler).
+
+-export([init/3]).
+-export([handle/2]).
+-export([terminate/3]).
+
+-define(FIELDS, [<<"appname">>]).
+
+init(_Transport, Req, []) ->
+    {ok, Req, undefined}.
+
+handle(Req, State) ->
+    {Method, Req2} = cowboy_req:method(Req),
+    HasBody = cowboy_req:has_body(Req2),
+    {ok, Req3} = handle_request(Method, HasBody, Req2),
+    {ok, Req3, State}.
+
+handle_request(<<"POST">>, true, Req) ->
+    {ok, Body, Req2} = cowboy_req:body_qs(Req),
+    _Application = application_from(Body),
+    cowboy_req:reply(201, [], <<"">>, Req2);
+
+handle_request(<<"POST">>, false, Req) ->
+    cowboy_req:reply(400, [], <<"{\"error\":\"missing body\"}">>, Req);
+
+handle_request(_, _, Req) ->
+    cowboy_req:reply(405, Req).
+
+terminate(_Reason, _Req, _State) ->
+    ok.
+
+application_from(_Body) ->
+    ok.
+    %%UserId = proplists:get_value(<<"userid">>, PostVals),
+    %%#application{appname=AppName}.
