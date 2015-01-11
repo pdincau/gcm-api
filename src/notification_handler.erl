5ac47502cc123c1020aec3af7a3404314d393ed1
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-01-11 20:42:48 +0100

    Using map instead of proplist

--- src/notification_handler.erl
+++ src/notification_handler.erl
@@ -18,8 +18,8 @@ handle(Req, State) ->
 handle_request(<<"POST">>, true, Req) ->
     {ok, [{Payload, true}], Req2} = cowboy_req:body_qs(Req),
     {AppName, Req3} = cowboy_req:binding(app_name, Req2),
-    Notification = notification_from(Payload),
-    process(AppName, jsx:decode(Payload)),
+    Update = update_from(Payload),
+    process(AppName, Update),
     cowboy_req:reply(201, [], <<"">>, Req3);
 
 handle_request(<<"POST">>, false, Req) ->
@@ -35,15 +35,22 @@ process(AppName, Update) ->
     error_logger:info_msg("Processing notifications: ~p~n", [Update]),
     lists:map(fun(Notification) -> send(AppName, Notification) end, Update).
 
-send(AppName, [{UserId, [{Key, Value}]}]) ->
+send(AppName, Notification) ->
+    [UserId] = maps:keys(Notification),
     case subscriptions:find(AppName, UserId) of
         {error, _} ->
             error_logger:error_msg("UserId: ~p not found.~n", [UserId]);
         {ok, Subscription} ->
             GCMAppName = binary_to_existing_atom(AppName, utf8),
-            Message = [{<<"data">>, [{Key, Value}]}],
+            Message = message_from(UserId, Notification),
             gcm:push(GCMAppName, [Subscription#subscription.regid], Message)
     end.
 
-notification_from(Payload) ->
+update_from(Payload) ->
     jsx:decode(Payload, [return_maps]).
+
+message_from(UserId, Notification) ->
+    UserDigest = maps:get(UserId, Notification),
+    [Key] = maps:keys(UserDigest),
+    Value = maps:get(Key, UserDigest),
+    [{<<"data">>, [{Key, Value}]}].
