989a08c0fec52582338a50c8ef8b0c1ae61b921b
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-01-11 21:01:53 +0100

    Adding add/1

--- src/applications.erl
+++ src/applications.erl
@@ -7,15 +7,25 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-export([add/1]).
+
 -define(SERVER, ?MODULE).
 
 -record(state, {}).
+
+add(Application) ->
+    error_logger:info_msg("Adding applicaton: ~p~n", [Application]),
+    gen_server:call(?SERVER, {add, Application}).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 init([]) ->
     {ok, #state{}}.
+
+handle_call({add, _Application}, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State};
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
