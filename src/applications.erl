15d7b5cf8165883e1501573fb32648ec1723a0a8
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-01-16 22:10:14 +0100

    Adding delete/1 function

--- src/applications.erl
+++ src/applications.erl
@@ -7,7 +7,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--export([add/1, get/1]).
+-export([add/1, get/1, delete/1]).
 
 -define(SERVER, ?MODULE).
 
@@ -18,6 +18,9 @@ add(Application) ->
 
 get(AppName) ->
     gen_server:call(?SERVER, {get, AppName}).
+
+delete(AppName) ->
+    gen_server:call(?SERVER, {delete, AppName}).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
@@ -40,6 +43,16 @@ handle_call({get, AppName}, _From, #state{connection=Connection} = State) ->
             {ok, jsx:decode(Json, [return_maps])}
     end,
     error_logger:info_msg("Get appname ~p. Result ~p~n", [AppName, Reply]),
+    {reply, Reply, State};
+
+handle_call({delete, AppName}, _From, #state{connection=Connection} = State) ->
+    Reply = case eredis:q(Connection, ["DEL", AppName]) of
+        {error, no_connection} ->
+            {error, no_connection};
+        {ok, Result} ->
+            {ok, Result}
+    end,
+    error_logger:info_msg("Remove application ~p. Result ~p~n", [Application, Reply]),
     {reply, Reply, State};
 
 handle_call(_Request, _From, State) ->
